f353fac4d06d8088ae8bedab1ea3691b7f48c830
Jaas00 <jaas2672@gmail.com>
2026-04-15 10:14:19 +0000

    Assignment[16]

--- SEMESTER-[1]/C-Programms/Secondary Assignments SEM -1/Assignment[16].c
+++ SEMESTER-[1]/C-Programms/Secondary Assignments SEM -1/Assignment[16].c
@@ -1,18 +1,59 @@
-/* Write a program that reads 10 integers into an array (using pointers), and prints the
-array in ascending and descending order. */
+/*Write a program that reads 10 integers into an array (using pointers), and prints the
+array in ascending and descending order.
+*/
 #include<stdio.h>
-void getdata(int *arr, int n) {
-    int i;
+void getdata(int*,int);
+void ascending(int*,int);
+void descending(int*,int);
+int main(){
+    int arr[10];
     printf("Enter the array elements : ");
-    for (i=0;i<n;i++)
-        scanf("%d",arr + i);
+    getdata(arr,10);
+    ascending(arr,10);
+    printf("\n");
+    descending(arr,10);
+    printf("\n");
+    return 0;
 }
-void ascending(int *arr, int n) {
-    int i,j,p = 1;
-    for (i=0;i<n && p==1;i++){
+void getdata(int *arr,int n){
+    int i;
+    for(i=0;i<n;i++){
+        scanf("%d",arr+i);
+    }
+}
+void ascending(int *arr, int n){
+    int i,j,p=1;
+    for(i=0;i<n&&p==1;i++){
         p=0;
-        for (j=0;j<n-1-i;j++) {
-            if ()
+        for(j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                arr[j] = arr[j]^arr[j+1];
+                arr[j+1] = arr[j]^arr[j+1];
+                arr[j] = arr[j]^arr[j+1];
+                p=1;
+            }
         }
+    }
+    printf("The array in ascending order is : ");
+    for(i=0;i<n;i++){
+        printf("%d ",*(arr+i));
+    }
+}
+void descending(int *arr, int n){
+    int i,j,p=1;
+    for(i=0;i<n&&p==1;i++){
+        p=0;
+        for(j=0;j<n-1-i;j++){
+            if(arr[j]<arr[j+1]){
+                arr[j] = arr[j]^arr[j+1];
+                arr[j+1] = arr[j]^arr[j+1];
+                arr[j] = arr[j]^arr[j+1];
+                p=1;
+            }
+        }
+    }
+    printf("The array in descending order is : ");
+    for(i=0;i<n;i++){
+        printf("%d ",*(arr+i));
     }
 }
