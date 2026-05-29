a7dd15df0f159b4bedadb82e4307a9bb6b36b5ef
Jaas <jaas2672@gmail.com>
2026-05-29 13:29:51 +0530

    p3

---
+++ SEMESTER-[1]/C-Programms/Let Us C/p3.c
@@ -0,0 +1,11 @@
+/* write a C program to print all the ASCII values and their equivalent characters using a while loop.
+The ASCII values vary from 0 to 255. */
+#include<stdio.h>
+int main(){
+    int i = 0;
+    while(i<=255){
+        printf("ASCII : %d and Equivalent Character : %c\n",i,i);
+        i++;
+    }
+    return 0;
+}
